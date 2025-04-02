9da6e865918144d58ef39d1a003857cc643c73da
gunnarguy <gunnarguy@me.com>
2025-04-01 18:36:26 -0700

    Initial Commit

---
+++ OpenCone/OpenConeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenConeApp.swift
+//  OpenCone
+//
+//  Created by Gunnar Hostetler on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct OpenConeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
